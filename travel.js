7345e381e9128a76538570bc8096ecb3c160f41e
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-05-01 23:43:20 +0545

    routung done only basic

---
+++ travel.js
@@ -0,0 +1,50 @@
+const express = require("express");
+var app = express();
+const handlebars = require("express-handlebars").create({
+  defaultLayout: "main",
+});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send("mad travel");
+});
+
+app.get("/about*", (req, res) => {
+  res.render("about");
+});
+
+app.get("/about/contact", (req, res) => {
+  res.type("text/plain");
+  res.send("About contacts tavel");
+});
+
+app.get("/about/directions", function (req, res) {
+  res.type("text/plain");
+  res.send("About directions tavel");
+});
+//custom 404 page
+app.use((req, res, next) => {
+  res.status(404);
+  res.render("404");
+  // next();
+});
+
+//custom 500 page
+
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500);
+  res.render("500");
+  // next();
+});
+
+app.listen(app.get("port"), () => {
+  console.log(
+    "Express started on http://localhost:" +
+      app.get("port") +
+      "; press Ctrl-C to terminate."
+  );
+});
